af4563f55f55bf1548656ca276541e410e70beb1
DESKTOP-6R1B181\user <woojoo0407@naver.com>
2025-02-23 16:10:50 +0900

    Link skill

--- Source/Rs/UI/ViewModel/RsAbilityViewModel.cpp
+++ Source/Rs/UI/ViewModel/RsAbilityViewModel.cpp
@@ -7,6 +7,11 @@
 
 URsAbilityViewModel* URsAbilityViewModel::CreateRsAbilityViewModel(URsGameplayAbility* Model)
 {
+	if (Model == nullptr)
+	{
+		return nullptr;
+	}
+	
 	URsAbilityViewModel* ViewModel = NewObject<URsAbilityViewModel>(Model);
 	ViewModel->Initialize();
 	return ViewModel;
