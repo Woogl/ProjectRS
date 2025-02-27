ce90e32e9994e32b933b2834407e16ef9c7965b4
문성훈 <foxtail96@naver.com>
2025-02-27 23:00:30 +0900

    MakeGameplayEvent GE Component Added

---
+++ Source/Rs/AbilitySystem/EffectComponent/MakeGameplayEventGameplayEffectComponent.cpp
@@ -0,0 +1,41 @@
+// Copyright 2024 Team BH.
+
+
+#include "MakeGameplayEventGameplayEffectComponent.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "Misc/DataValidation.h"
+
+#define LOCTEXT_NAMESPACE "SendEventToTargetGameplayEffectComponent"
+
+void UMakeGameplayEventGameplayEffectComponent::OnGameplayEffectApplied(FActiveGameplayEffectsContainer& ActiveGEContainer, FGameplayEffectSpec& GESpec,FPredictionKey& PredictionKey) const
+{
+	if (!ensureMsgf(ActiveGEContainer.Owner, TEXT("OnGameplayEffectApplied is passed an ActiveGEContainer which lives within an ASC but that ASC was somehow null")))
+	{
+		return;
+	}
+
+	if (!EventTags.IsEmpty())
+	{
+		for (FGameplayTag EventTag : EventTags)
+		{
+			FGameplayEventData Payload;
+			UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(ActiveGEContainer.Owner->GetOwnerActor(),EventTag,Payload);
+		}
+	}
+}
+
+#if WITH_EDITOR
+EDataValidationResult UMakeGameplayEventGameplayEffectComponent::IsDataValid(class FDataValidationContext& Context) const
+{
+	EDataValidationResult Result = Super::IsDataValid(Context);
+	if (EventTags.IsEmpty())
+	{
+		Context.AddWarning(LOCTEXT("EventWithNoParams", "Sending Gameplay Event to Target with no params. It won't operate any GA."));
+		Result = EDataValidationResult::Invalid;
+	}
+	return Result;
+}
+#endif
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/Rs/AbilitySystem/EffectComponent/MakeGameplayEventGameplayEffectComponent.h
@@ -0,0 +1,37 @@
+// Copyright 2024 Team BH.
+
+#pragma once
+
+
+#include "GameplayEffectComponent.h"
+#include "GameplayTagContainer.h"
+#include "MakeGameplayEventGameplayEffectComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS(DisplayName="Make Gameplay Event")
+class RS_API UMakeGameplayEventGameplayEffectComponent : public UGameplayEffectComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	/**
+	 * Called when a Gameplay Effect is initially applied, or stacked.  GE's are 'applied' in both cases of duration or instant execution.  This call does not happen periodically, nor through replication.
+	 * One should favor this function over OnActiveGameplayEffectAdded & OnGameplayEffectExecuted (but all multiple may be used depending on the case).
+	 */
+	virtual void OnGameplayEffectApplied(FActiveGameplayEffectsContainer& ActiveGEContainer, FGameplayEffectSpec& GESpec, FPredictionKey& PredictionKey) const override;
+
+#if WITH_EDITOR
+	/**
+	 * Allow each Gameplay Effect Component to validate its own data.  Any warnings/errors will immediately show up in the Gameplay Effect when in Editor.
+	 * The default implementation ensures we only have a single type of any given class.  Override this function to change that functionality and use Super::Super::IsDataValid if needed.
+	 */
+	virtual EDataValidationResult IsDataValid(class FDataValidationContext& Context) const override;
+#endif
+
+public:
+	UPROPERTY(EditDefaultsOnly, Category = Tags, meta = (DisplayName = "EventTags"))
+	FGameplayTagContainer EventTags;
+};
