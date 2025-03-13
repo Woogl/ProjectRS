ef22b2c477483d2be9f9de26ed19746894059fe4
DESKTOP-6R1B181\user <woojoo0407@naver.com>
2025-03-14 00:12:38 +0900

    AbilityTask WaitEnhancedInput

---
+++ Source/Rs/AbilitySystem/AbilityTask/RsAbilityTask_WaitEnhancedInput.cpp
@@ -0,0 +1,60 @@
+// Copyright 2024 Team BH.
+
+
+#include "RsAbilityTask_WaitEnhancedInput.h"
+
+#include "EnhancedInputComponent.h"
+
+URsAbilityTask_WaitEnhancedInput* URsAbilityTask_WaitEnhancedInput::WaitEnhancedInput(UGameplayAbility* OwningAbility, const FName TaskInstanceName, UInputAction* InputAction, bool bShouldOnlyTriggerOnce)
+{
+	URsAbilityTask_WaitEnhancedInput* AbilityTask = NewAbilityTask<URsAbilityTask_WaitEnhancedInput>(OwningAbility, TaskInstanceName);
+	AbilityTask->InputAction = InputAction;
+	AbilityTask->bTriggerOnce = bShouldOnlyTriggerOnce;
+	return AbilityTask;
+}
+
+void URsAbilityTask_WaitEnhancedInput::Activate()
+{
+	Super::Activate();
+	
+	if (!AbilitySystemComponent.Get() || !Ability || !InputAction.IsValid())
+	{
+		return;
+	}
+
+	const APawn* const AvatarPawn = Cast<APawn>(Ability->GetAvatarActorFromActorInfo());
+	const APlayerController* const PlayerController = AvatarPawn ? Cast<APlayerController>(AvatarPawn->GetController()) : nullptr;
+	
+	if (!PlayerController)
+	{
+		return;
+	}
+
+	EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerController->InputComponent);
+			
+	if (IsValid(EnhancedInputComponent.Get()))
+	{
+		EnhancedInputComponent->BindAction(InputAction.Get(), ETriggerEvent::Triggered, this, &ThisClass::HandleInputAction);
+	}
+}
+
+void URsAbilityTask_WaitEnhancedInput::HandleInputAction(const FInputActionValue& Value)
+{
+	if (bTriggerOnce && bHasBeenTriggered)
+	{
+		return;
+	}
+	bHasBeenTriggered = true;
+	
+	InputEventReceived.Broadcast(Value);
+}
+
+void URsAbilityTask_WaitEnhancedInput::OnDestroy(const bool bInOwnerFinished)
+{
+	if (IsValid(EnhancedInputComponent.Get()))
+	{
+		EnhancedInputComponent->ClearBindingsForObject(this);
+	}
+	
+	Super::OnDestroy(bInOwnerFinished);
+}
---
+++ Source/Rs/AbilitySystem/AbilityTask/RsAbilityTask_WaitEnhancedInput.h
@@ -0,0 +1,38 @@
+// Copyright 2024 Team BH.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputActionValue.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "RsAbilityTask_WaitEnhancedInput.generated.h"
+
+class UInputAction;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEnhancedInputEventDelegate, FInputActionValue, Value);
+
+/**
+ * 
+ */
+UCLASS()
+class RS_API URsAbilityTask_WaitEnhancedInput : public UAbilityTask
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FEnhancedInputEventDelegate InputEventReceived;
+	
+	UFUNCTION(BlueprintCallable, meta = (HidePin = "OwningAbility", DefaultToSelf = "OwningAbility", BlueprintInternalUseOnly = "TRUE"), Category = "Ability Tasks")
+	static URsAbilityTask_WaitEnhancedInput* WaitEnhancedInput(UGameplayAbility* OwningAbility, const FName TaskInstanceName, UInputAction* InputAction, bool bShouldOnlyTriggerOnce = true);
+
+private:
+	TWeakObjectPtr<UEnhancedInputComponent> EnhancedInputComponent = nullptr;
+	TWeakObjectPtr<UInputAction> InputAction = nullptr;
+	bool bTriggerOnce = true;
+	bool bHasBeenTriggered = false;
+
+	virtual void Activate() override;
+	void HandleInputAction(const FInputActionValue& Value);
+	virtual void OnDestroy(const bool bInOwnerFinished) override;
+};
