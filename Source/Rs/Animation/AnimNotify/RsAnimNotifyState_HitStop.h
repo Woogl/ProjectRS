0760072e17c9a10114ec98183351d742c1e0d5bb
Woogl <106436076+Woogl@users.noreply.github.com>
2025-03-15 05:18:57 +0900

    Update RsAnimNotifyState_HitStop.h

--- Source/Rs/Animation/AnimNotify/RsAnimNotifyState_HitStop.h
+++ Source/Rs/Animation/AnimNotify/RsAnimNotifyState_HitStop.h
@@ -23,8 +23,6 @@ public:
 	float Duration = 0.25f;
 
 private:
-	virtual FString GetNotifyName_Implementation() const override;
-	
 	virtual void NotifyBegin(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
 	virtual void NotifyEnd(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, const FAnimNotifyEventReference& EventReference) override;
 
