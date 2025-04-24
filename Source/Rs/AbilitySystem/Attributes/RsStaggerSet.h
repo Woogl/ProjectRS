55935af67f4094ac5bef4edc7484a040e0718c7c
DESKTOP-6R1B181\user <woojoo0407@naver.com>
2025-04-24 21:44:16 +0900

    GE_StaggerDamage

--- Source/Rs/AbilitySystem/Attributes/RsStaggerSet.h
+++ Source/Rs/AbilitySystem/Attributes/RsStaggerSet.h
@@ -17,33 +17,31 @@ class RS_API URsStaggerSet : public URsAttributeSetBase
 public:
 	URsStaggerSet();
 
-	// Attribute Set Overrides.
 	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
 	virtual void PostAttributeChange(const FGameplayAttribute& Attribute, float OldValue, float NewValue) override;
 	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
-
-	// Set Attributes to replicate.
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 	
-	// Used to create a local copy of stagger damage which is then added to Current Stagger.
 	UPROPERTY(BlueprintReadOnly)
 	FGameplayAttributeData StaggerDamage;
 	ATTRIBUTE_ACCESSORS(URsStaggerSet, StaggerDamage)
 	
-	// Holds the current value for Stagger.
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_CurrentStagger)
 	FGameplayAttributeData CurrentStagger;
 	ATTRIBUTE_ACCESSORS(URsStaggerSet, CurrentStagger)
 
-	// Holds the value for Maximum Stagger.
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxStagger)
 	FGameplayAttributeData MaxStagger;
 	ATTRIBUTE_ACCESSORS(URsStaggerSet, MaxStagger)
 
-	// Holds the current value for Stagger Regeneration.
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_StaggerRegen)
 	FGameplayAttributeData StaggerRegen;
 	ATTRIBUTE_ACCESSORS(URsStaggerSet, StaggerRegen)
+
+	// Meta attribute for RsStaggerDamageCalculation
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayAttributeData BaseDamage;
+	ATTRIBUTE_ACCESSORS(URsStaggerSet, BaseDamage)
 
 protected:
 	UFUNCTION()
--- Source/Rs/AbilitySystem/Calculation/Execution/RsStaggerDamageCalculation.cpp
+++ Source/Rs/AbilitySystem/Calculation/Execution/RsStaggerDamageCalculation.cpp
@@ -3,23 +3,18 @@
 
 #include "RsStaggerDamageCalculation.h"
 
-#include "Rs/AbilitySystem/Attributes/RsAttackSet.h"
-#include "Rs/AbilitySystem/Attributes/RsDefenseSet.h"
 #include "Rs/AbilitySystem/Attributes/RsStaggerSet.h"
-#include "Rs/System/RsGameSetting.h"
 
 // Declare the attributes to capture and define how we want to capture them from the Source and Target.
 struct RsStaggerStatics
 {
-	DECLARE_ATTRIBUTE_CAPTUREDEF(Impact);
-	DECLARE_ATTRIBUTE_CAPTUREDEF(CurrentStagger);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(BaseDamage);
 	DECLARE_ATTRIBUTE_CAPTUREDEF(StaggerDamage);
 
 	RsStaggerStatics()
 	{
 		// Capture optional attribute set
-		DEFINE_ATTRIBUTE_CAPTUREDEF(URsAttackSet, Impact, Source, false);
-		DEFINE_ATTRIBUTE_CAPTUREDEF(URsStaggerSet, CurrentStagger, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(URsStaggerSet, BaseDamage, Target, false);
 		DEFINE_ATTRIBUTE_CAPTUREDEF(URsStaggerSet, StaggerDamage, Target, false);
 	}
 
@@ -33,9 +28,8 @@ struct RsStaggerStatics
 URsStaggerDamageCalculation::URsStaggerDamageCalculation()
 {
 	const RsStaggerStatics* DamageStatics = &RsStaggerStatics::Get();
-	
-	RelevantAttributesToCapture.Add(DamageStatics->ImpactDef);
-	RelevantAttributesToCapture.Add(DamageStatics->CurrentStaggerDef);
+
+	RelevantAttributesToCapture.Add(DamageStatics->BaseDamageDef);
 }
 
 void URsStaggerDamageCalculation::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
@@ -52,20 +46,18 @@ void URsStaggerDamageCalculation::Execute_Implementation(const FGameplayEffectCu
 
 	const RsStaggerStatics* DamageStatics = &RsStaggerStatics::Get();
 
-	// Set in RsGameplayAbility_Attack
-	float StaggerCoefficient = FMath::Max<float>(Spec.GetSetByCallerMagnitude(URsGameSetting::Get()->StaggerDamageCoefficientTag, true, 1.f), 0.f);
-
-	float Impact = 0.f;
-	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics->ImpactDef, EvaluationParameters, Impact);
+	// Set in RsCoefficientCalculation
+	float BaseDamage = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics->BaseDamageDef, EvaluationParameters, BaseDamage);
 	
 	// Stagger Calculation
-	float FinalStaggerDamage = Impact * StaggerCoefficient;
+	float FinalDamage = BaseDamage;
 
-	if (FinalStaggerDamage <= 0.f)
+	if (FinalDamage <= 0.f)
 	{
 		return;
 	}
 
 	OutExecutionOutput.MarkConditionalGameplayEffectsToTrigger();
-	OutExecutionOutput.AddOutputModifier(FGameplayModifierEvaluatedData(DamageStatics->StaggerDamageProperty, EGameplayModOp::Additive, FinalStaggerDamage));
+	OutExecutionOutput.AddOutputModifier(FGameplayModifierEvaluatedData(DamageStatics->StaggerDamageProperty, EGameplayModOp::Additive, FinalDamage));
 }
