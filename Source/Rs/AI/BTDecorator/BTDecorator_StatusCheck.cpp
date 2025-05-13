2b0434121452f138c0ee6903346276c3053c6ea0
문성훈 <foxtail96@naver.com>
2025-05-14 00:16:41 +0900

    BTDecorator_StatusCheck Added

---
+++ Source/Rs/AI/BTDecorator/BTDecorator_StatusCheck.cpp
@@ -0,0 +1,35 @@
+// Copyright 2024 Team BH.
+
+
+#include "BTDecorator_StatusCheck.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+#include "AIController.h"
+
+bool UBTDecorator_StatusCheck::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	UAbilitySystemComponent* ASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(OwnerComp.GetAIOwner());
+	if (ASC)
+	{
+		bool FoundResult;
+		float FoundValue = ASC->GetGameplayAttributeValue(Attribute, FoundResult);
+
+		if (FoundResult)
+		{
+			switch (Operator)
+			{
+			case EAttributeInequalityOperator::Equal:
+				return FMath::IsNearlyEqual(FoundValue, Value, 0.4);
+			case EAttributeInequalityOperator::Less:
+				return FoundValue < Value;
+			case EAttributeInequalityOperator::Less_Equal:
+				return FoundValue <= Value;
+			case EAttributeInequalityOperator::Greater:
+				return FoundValue > Value;
+			case EAttributeInequalityOperator::Greater_Equal:
+				return FoundValue >= Value;
+			}
+		}
+	}
+	return false;
+}
---
+++ Source/Rs/AI/BTDecorator/BTDecorator_StatusCheck.h
@@ -0,0 +1,41 @@
+// Copyright 2024 Team BH.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "BTDecorator_StatusCheck.generated.h"
+
+/**
+ * 
+ */
+UENUM(BlueprintType)
+enum class EAttributeInequalityOperator : uint8
+{
+	Equal,
+	Less,
+	Less_Equal,
+	Greater,
+	Greater_Equal,
+};
+
+UCLASS()
+class RS_API UBTDecorator_StatusCheck : public UBTDecorator
+{
+	GENERATED_BODY()
+
+public:
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FGameplayAttribute Attribute;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	EAttributeInequalityOperator Operator;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Value;
+
+	
+};
