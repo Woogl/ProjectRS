cbcd1f376bf10b580e619d598fa087154b4d6b74
문성훈 <foxtail96@naver.com>
2025-05-17 03:00:28 +0900

    TagCheck Decorator, using example (in BT_EnemyCharacter) added

---
+++ Source/Rs/AI/BTDecorator/BTDecorator_TagCheck.cpp
@@ -0,0 +1,49 @@
+// Copyright 2024 Team BH.
+
+
+#include "BTDecorator_TagCheck.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+#include "AIController.h"
+
+UBTDecorator_TagCheck::UBTDecorator_TagCheck()
+{
+	bNotifyBecomeRelevant = true;
+	bNotifyCeaseRelevant = true;
+}
+
+bool UBTDecorator_TagCheck::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	if (ASC.IsValid() && ASC->HasMatchingGameplayTag(Tag))
+	{
+		return true;
+	}
+	return false;
+}
+
+void UBTDecorator_TagCheck::OnTagAddedOrRemoved(const FGameplayTag, int32 Stack, UBehaviorTreeComponent* OwnerComp)
+{
+	if (IsValid(OwnerComp))
+	{
+		OwnerComp->RequestExecution(this);
+	}
+}
+
+void UBTDecorator_TagCheck::OnBecomeRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	Super::OnBecomeRelevant(OwnerComp, NodeMemory);
+	ASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(OwnerComp.GetAIOwner()->GetPawn());
+	if (ASC.IsValid())
+	{
+		ASC->RegisterGameplayTagEvent(Tag).AddUObject(this, &UBTDecorator_TagCheck::OnTagAddedOrRemoved, &OwnerComp);
+	}
+}
+
+void UBTDecorator_TagCheck::OnCeaseRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	if (ASC.IsValid())
+	{
+		ASC->RegisterGameplayTagEvent(Tag).RemoveAll(this);
+	}
+	Super::OnCeaseRelevant(OwnerComp, NodeMemory);
+}
---
+++ Source/Rs/AI/BTDecorator/BTDecorator_TagCheck.h
@@ -0,0 +1,37 @@
+// Copyright 2024 Team BH.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "BTDecorator_TagCheck.generated.h"
+
+/**
+ * 
+ */
+class UAbilitySystemComponent;
+
+UCLASS()
+class RS_API UBTDecorator_TagCheck : public UBTDecorator
+{
+	GENERATED_BODY()
+	
+public:
+	UBTDecorator_TagCheck();
+	
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+
+	void OnTagAddedOrRemoved(const FGameplayTag ChangedTag, int32 Stack, UBehaviorTreeComponent* OwnerComp);
+	
+protected:
+	virtual void OnBecomeRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	virtual void OnCeaseRelevant(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Tag To Check (if exists, return true)"))
+	FGameplayTag Tag;
+
+protected:
+	UPROPERTY()
+	TWeakObjectPtr<UAbilitySystemComponent> ASC;
+};
