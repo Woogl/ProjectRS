46f9cca85daebae37f3525c4b4bf71569806fb8f
DESKTOP-6R1B181\user <woojoo0407@naver.com>
2025-06-14 02:53:10 +0900

    AN_Teleport

---
+++ Source/Rs/Animation/AnimNotify/RsAnimNotify_Teleport.cpp
@@ -0,0 +1,108 @@
+﻿// Copyright 2025 Team BH.
+
+
+#include "RsAnimNotify_Teleport.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Character.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Rs/Camera/LockOn/RsLockOnComponent.h"
+#include "Rs/Character/RsEnemyCharacter.h"
+#include "Rs/Player/RsPlayerController.h"
+
+void URsAnimNotify_Teleport::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+
+	AActor* Owner = MeshComp->GetOwner();
+	if (!Owner)
+	{
+		return;
+	}
+
+	AActor* TeleportTarget = nullptr;
+	
+	if (ACharacter* Character = Cast<ACharacter>(Owner))
+	{
+		if (AController* Controller = Character->GetController())
+		{
+			// PlayerCharacter use LockOnComp target.
+			if (ARsPlayerController* RsPlayerController = Cast<ARsPlayerController>(Controller))
+			{
+				if (URsLockOnComponent* LockOnComponent = RsPlayerController->GetLockOnComponent())
+				{
+					TeleportTarget = LockOnComponent->GetLockedOnTarget();
+
+					// If the lock on target is none, Search the closest enemy.
+					if (TeleportTarget == nullptr)
+					{
+						TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
+						ObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_Pawn));
+						TArray<AActor*> OutActors;
+						UKismetSystemLibrary::SphereOverlapActors(Owner->GetWorld(), Owner->GetActorLocation(), MaxSearchDistance, ObjectTypes, ARsEnemyCharacter::StaticClass(), TArray<AActor*>(), OutActors);
+
+						const FVector OwnerLocation = Owner->GetActorLocation();
+						float MinDistanceSquared = MaxSearchDistance * MaxSearchDistance;
+						for (AActor* Actor : OutActors)
+						{
+							float DistanceSquared = FVector::DistSquared(Actor->GetActorLocation(), OwnerLocation);
+							if (DistanceSquared < MinDistanceSquared)
+							{
+								MinDistanceSquared = DistanceSquared;
+								TeleportTarget = Actor;
+							}
+						}
+					}
+				}
+			}
+			
+			// EnemyCharacter use AI BlackBoard target.
+			else if (AAIController* AIController = Cast<AAIController>(Controller))
+			{
+				if (UBlackboardComponent* BBComponent = AIController->GetBlackboardComponent())
+				{
+					if (UObject* BBObject = BBComponent->GetValueAsObject(TEXT("TargetActor")))
+					{
+						TeleportTarget = Cast<AActor>(BBObject);
+					}
+				}
+			}
+		}
+	}
+
+	FVector TeleportDestination = Position;
+	if (PositionMode == ERsTeleportPosition::WorldPosition)
+	{
+		// Teleport to World Position.
+		Owner->SetActorLocation(TeleportDestination);
+	}
+	else if (PositionMode == ERsTeleportPosition::LocalPosition)
+	{
+		if (TeleportTarget == nullptr)
+		{
+			return;
+		}
+		FRotator TargetRotation = TeleportTarget->GetActorRotation();
+		TeleportDestination = TeleportTarget->GetActorLocation() + TargetRotation.RotateVector(Position);
+		// Teleport to Local Position.
+		Owner->SetActorLocation(TeleportDestination);
+	}
+	
+	if (bLookTarget == true)
+	{
+		FVector LookDirection;
+		if (TeleportTarget)
+		{
+			LookDirection = TeleportTarget->GetActorLocation() - Owner->GetActorLocation();	
+		}
+		else
+		{
+			LookDirection = TeleportDestination - Owner->GetActorLocation();
+		}
+		
+		FRotator LookAtRotation = LookDirection.Rotation();
+		FRotator OriginalRotation = Owner->GetActorRotation();
+		Owner->SetActorRotation(FRotator(OriginalRotation.Pitch, LookAtRotation.Yaw, OriginalRotation.Roll));
+	}
+}
---
+++ Source/Rs/Animation/AnimNotify/RsAnimNotify_Teleport.h
@@ -0,0 +1,37 @@
+﻿// Copyright 2025 Team BH.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "RsAnimNotify_Teleport.generated.h"
+
+UENUM()
+enum class ERsTeleportPosition : uint8
+{
+	LocalPosition,
+	WorldPosition
+};
+/**
+ * 
+ */
+UCLASS()
+class RS_API URsAnimNotify_Teleport : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	ERsTeleportPosition PositionMode = ERsTeleportPosition::LocalPosition;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector Position = FVector::ZeroVector;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bLookTarget = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MaxSearchDistance = 10000.f;
+	
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+};
