32b97a76268749436818c8465b0c85339702a89b
DESKTOP-6R1B181\user <woojoo0407@naver.com>
2025-08-15 22:17:59 +0900

    Fix bug on Triple Link Skill click

---
+++ Source/Rs/UI/Widget/RsInputActionWidget.cpp
@@ -0,0 +1,5 @@
+// Copyright 2025 Team BH.
+
+
+#include "RsActionWidget.h"
+
---
+++ Source/Rs/UI/Widget/RsInputActionWidget.h
@@ -0,0 +1,17 @@
+// Copyright 2025 Team BH.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActionWidget.h"
+#include "RsActionWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RS_API URsActionWidget : public UCommonActionWidget
+{
+	GENERATED_BODY()
+	
+};
