d91fd33da25160c2c879158c801b1b09285d9eda
DESKTOP-6R1B181\user <woojoo0407@naver.com>
2025-09-06 17:13:38 +0900

    RsGameplayStatics

---
+++ Source/Rs/System/RsGameplayStatics.cpp
@@ -0,0 +1,35 @@
+﻿// Copyright 2025 Team BH.
+
+
+#include "RsGameplayStatics.h"
+
+#include "Rs/Character/RsPlayerCharacter.h"
+#include "Rs/Player/RsPlayerController.h"
+#include "Rs/Player/RsPlayerState.h"
+
+ARsPlayerState* URsGameplayStatics::GetRsPlayerState(const UObject* WorldContextObject, int32 PlayerIndex)
+{
+	if (APlayerState* PlayerState = GetPlayerState(WorldContextObject, PlayerIndex))
+	{
+		return Cast<ARsPlayerState>(PlayerState);
+	}
+	return nullptr;
+}
+
+ARsPlayerController* URsGameplayStatics::GetRsPlayerController(const UObject* WorldContextObject, int32 PlayerIndex)
+{
+	if (APlayerController* PlayerController = GetPlayerController(WorldContextObject, PlayerIndex))
+	{
+		return Cast<ARsPlayerController>(PlayerController);
+	}
+	return nullptr;
+}
+
+ARsPlayerCharacter* URsGameplayStatics::GetRsPlayerCharacter(const UObject* WorldContextObject, int32 PlayerIndex)
+{
+	if (ACharacter* PlayerCharacter = GetPlayerCharacter(WorldContextObject, PlayerIndex))
+	{
+		return Cast<ARsPlayerCharacter>(PlayerCharacter);
+	}
+	return nullptr;
+}
---
+++ Source/Rs/System/RsGameplayStatics.h
@@ -0,0 +1,29 @@
+﻿// Copyright 2025 Team BH.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/GameplayStatics.h"
+#include "RsGameplayStatics.generated.h"
+
+class ARsPlayerState;
+class ARsPlayerController;
+class ARsPlayerCharacter;
+/**
+ * 
+ */
+UCLASS()
+class RS_API URsGameplayStatics : public UGameplayStatics
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintPure, Category = "RS", meta = (WorldContext = "WorldContextObject", UnsafeDuringActorConstruction="true"))
+	static ARsPlayerState* GetRsPlayerState(const UObject* WorldContextObject, int32 PlayerIndex);
+
+	UFUNCTION(BlueprintPure, Category="RS", meta=(WorldContext="WorldContextObject", UnsafeDuringActorConstruction="true"))
+	static ARsPlayerController* GetRsPlayerController(const UObject* WorldContextObject, int32 PlayerIndex);
+
+	UFUNCTION(BlueprintPure, Category="RS", meta=(WorldContext="WorldContextObject", UnsafeDuringActorConstruction="true"))
+	static ARsPlayerCharacter* GetRsPlayerCharacter(const UObject* WorldContextObject, int32 PlayerIndex);
+};
