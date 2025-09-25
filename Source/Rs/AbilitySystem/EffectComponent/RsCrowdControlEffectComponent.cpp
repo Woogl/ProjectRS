24b5f0117b76c02b913b705c20f46a751eb9f435
DESKTOP-6R1B181\user <woojoo0407@naver.com>
2025-09-25 23:57:00 +0900

    Crowd control

---
+++ Source/Rs/AbilitySystem/EffectComponent/RsCrowdControlEffectComponent.cpp
@@ -0,0 +1,32 @@
+﻿// Copyright 2025 Team BH.
+
+
+#include "RsCrowdControlEffectComponent.h"
+
+#include "AbilitySystemComponent.h"
+#include "GameplayEffect.h"
+#include "Abilities/GameplayAbilityTypes.h"
+
+void URsCrowdControlEffectComponent::OnGameplayEffectApplied(FActiveGameplayEffectsContainer& ActiveGEContainer, FGameplayEffectSpec& GESpec, FPredictionKey& PredictionKey) const
+{
+	Super::OnGameplayEffectApplied(ActiveGEContainer, GESpec, PredictionKey);
+	
+	if (!ensureMsgf(ActiveGEContainer.Owner, TEXT("OnGameplayEffectApplied is passed an ActiveGEContainer which lives within an ASC but that ASC was somehow null")))
+	{
+		return;
+	}
+
+	if (EventTag.IsValid())
+	{
+		FGameplayEventData Payload;
+		Payload.EventTag = EventTag;
+		Payload.Instigator = GESpec.GetEffectContext().GetInstigator();
+		Payload.Target = GESpec.GetEffectContext().GetHitResult()->GetActor();
+		Payload.InstigatorTags = GESpec.CapturedSourceTags.GetActorTags();
+		Payload.TargetTags = GESpec.CapturedTargetTags.GetActorTags();
+		Payload.EventMagnitude = GESpec.GetLevel();
+		Payload.ContextHandle = GESpec.GetEffectContext();
+			
+		ActiveGEContainer.Owner->HandleGameplayEvent(EventTag, &Payload);
+	}
+}
---
+++ Source/Rs/AbilitySystem/EffectComponent/RsCrowdControlEffectComponent.h
@@ -0,0 +1,26 @@
+﻿// Copyright 2025 Team BH.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectComponent.h"
+#include "GameplayTagContainer.h"
+#include "RsCrowdControlEffectComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RS_API URsCrowdControlEffectComponent : public UGameplayEffectComponent
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, meta = (Categories = "Ability.HitReaction", ForceInlineRow))
+	FGameplayTag EventTag;
+
+	// UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (RowType = ""))
+	// FDataTableRowHandle DataTableRow;
+	
+public:
+	virtual void OnGameplayEffectApplied(FActiveGameplayEffectsContainer& ActiveGEContainer, FGameplayEffectSpec& GESpec, FPredictionKey& PredictionKey) const override;
+};
