b3751dd10dd0c77533af7a590e6f66031960e0ed
DESKTOP-6R1B181\user <woojoo0407@naver.com>
2025-11-04 01:25:56 +0900

    Additional Source GE Comp

---
+++ Source/Rs/AbilitySystem/EffectComponent/RsAdditionalSourceEffectComponent.cpp
@@ -0,0 +1,154 @@
+﻿// Copyright 2025 Team BH.
+
+
+#include "RsAdditionalSourceEffectComponent.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemLog.h"
+#include "GameplayEffect.h"
+#include "Misc/DataValidation.h"
+
+#define LOCTEXT_NAMESPACE "RsAdditionalSourceEffectComponent"
+
+bool URsAdditionalSourceEffectComponent::OnActiveGameplayEffectAdded(FActiveGameplayEffectsContainer& ActiveGEContainer, FActiveGameplayEffect& ActiveGE) const
+{
+	// We don't allow prediction of expiration (on removed) effects
+	if (ActiveGEContainer.IsNetAuthority())
+	{
+		// When this ActiveGE gets removed, so will our events so no need to unbind this.
+		ActiveGE.EventSet.OnEffectRemoved.AddUObject(this, &ThisClass::OnActiveGameplayEffectRemoved, &ActiveGEContainer);
+	}
+
+	return true;
+}
+
+void URsAdditionalSourceEffectComponent::OnGameplayEffectApplied(FActiveGameplayEffectsContainer& ActiveGEContainer, FGameplayEffectSpec& GESpec, FPredictionKey& PredictionKey) const
+{
+	if (!ensureMsgf(ActiveGEContainer.Owner, TEXT("OnGameplayEffectApplied is passed an ActiveGEContainer which lives within an ASC but that ASC was somehow null")))
+	{
+		return;
+	}
+
+	const float GELevel = GESpec.GetLevel();
+	const FGameplayEffectContextHandle& GEContextHandle = GESpec.GetEffectContext();
+
+	/** other effects that need to be applied to the source if this effect is successful */
+	TArray<FGameplayEffectSpecHandle> SourceEffectSpecs;
+	for (const TSubclassOf<UGameplayEffect>& EffectClass : OnApplicationGameplayEffects)
+	{
+		const UGameplayEffect* GameplayEffectDef = EffectClass.GetDefaultObject();
+		if (!GameplayEffectDef)
+		{
+			continue;
+		}
+
+		FGameplayEffectSpecHandle SpecHandle = GESpec.GetEffectContext().GetInstigatorAbilitySystemComponent()->MakeOutgoingSpec(EffectClass, GELevel, GEContextHandle);
+		if (SpecHandle.IsValid())
+		{
+			SourceEffectSpecs.Add(SpecHandle);
+		}
+	}
+
+	// Add all of the dynamically linked ones that don't have any conditions applied to them
+	// Note: I believe this functionality should be removed as its usage is dynamic and thus not 
+	// synchronized between client/server.  I've deprecated the variable to help enforce this.
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+	SourceEffectSpecs.Append(GESpec.TargetEffectSpecs);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+	// Apply to source!!!
+	UAbilitySystemComponent& AppliedToASC = *GESpec.GetContext().GetInstigatorAbilitySystemComponent();
+	bool bHasApplied = false;
+	for (const FGameplayEffectSpecHandle& SourceSpec : SourceEffectSpecs)
+	{
+		if (SourceSpec.IsValid())
+		{
+			// if (bApplyOnce && bHasApplied)
+			// {
+			// 	break;
+			// }
+			
+			FActiveGameplayEffectHandle Handle = AppliedToASC.ApplyGameplayEffectSpecToSelf(*SourceSpec.Data.Get(), PredictionKey);
+			if (Handle.IsValid())
+			{
+				bHasApplied = true;
+			}
+		}
+	}
+}
+
+void URsAdditionalSourceEffectComponent::OnActiveGameplayEffectRemoved(const FGameplayEffectRemovalInfo& RemovalInfo, FActiveGameplayEffectsContainer* ActiveGEContainer) const
+{
+	FScopedActiveGameplayEffectLock ActiveScopeLock(*ActiveGEContainer);
+	
+	const FActiveGameplayEffect* ActiveGE = RemovalInfo.ActiveEffect;
+	if (!ensure(ActiveGE))
+	{
+		UE_LOG(LogGameplayEffects, Error, TEXT("FGameplayEffectRemovalInfo::ActiveEffect was not populated in OnActiveGameplayEffectRemoved"));
+		return;
+	}
+
+	// Apply to source!!!
+	UAbilitySystemComponent* ASC = RemovalInfo.EffectContext.GetInstigatorAbilitySystemComponent();
+	if (!ASC)
+	{
+		UE_LOG(LogGameplayEffects, Verbose, TEXT("InstigatorAbilitySystemComponent was invalid in OnActiveGameplayEffectRemoved"));
+		return;
+	}
+
+	// Determine the appropriate type of effect to apply depending on whether the effect is being prematurely removed or not
+	const TArray<TSubclassOf<UGameplayEffect>>& ExpiryEffects = (RemovalInfo.bPrematureRemoval ? OnCompletePrematurely : OnCompleteNormal);
+
+	// Mix-in the always-executing GameplayEffects
+	TArray<TSubclassOf<UGameplayEffect>> AllGameplayEffects{ ExpiryEffects };
+	AllGameplayEffects.Append(OnCompleteAlways);
+
+	bool bHasApplied = false;
+	for (TSubclassOf<UGameplayEffect>& CurExpiryEffect : AllGameplayEffects)
+	{
+		// if (bApplyOnce && bHasApplied)
+		// {
+		// 	break;
+		// }
+		
+		if (UGameplayEffect* CurExpiryCDO = CurExpiryEffect.GetDefaultObject())
+		{
+			FGameplayEffectSpec NewSpec;
+			NewSpec.InitializeFromLinkedSpec(CurExpiryCDO, ActiveGE->Spec);
+
+			FActiveGameplayEffectHandle Handle = ASC->ApplyGameplayEffectSpecToSelf(NewSpec);
+			if (Handle.IsValid())
+			{
+				bHasApplied = true;
+			}
+		}
+	}
+}
+
+#if WITH_EDITOR
+EDataValidationResult URsAdditionalSourceEffectComponent::IsDataValid(class FDataValidationContext& Context) const
+{
+	EDataValidationResult Result = Super::IsDataValid(Context);
+
+	if (GetOwner()->DurationPolicy == EGameplayEffectDurationType::Instant)
+	{
+		const bool bHasOnCompleteEffects = (OnCompleteAlways.Num() + OnCompleteNormal.Num() + OnCompletePrematurely.Num() > 0);
+		if (bHasOnCompleteEffects)
+		{
+			Context.AddError(FText::FormatOrdered(LOCTEXT("InstantDoesNotWorkWithOnComplete", "Instant GE will never receive OnComplete for {0}."), FText::FromString(GetClass()->GetName())));
+			Result = EDataValidationResult::Invalid;
+		}
+	}
+	else if (GetOwner()->Period.Value > 0.0f)
+	{
+		if (OnApplicationGameplayEffects.Num() > 0)
+		{
+			Context.AddWarning(LOCTEXT("IsPeriodicAndHasOnApplication", "Periodic GE has OnApplicationGameplayEffects. Those GE's will only be applied once."));
+		}
+	}
+
+	return Result;
+}
+#endif // WITH_EDITOR
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/Rs/AbilitySystem/EffectComponent/RsAdditionalSourceEffectComponent.h
@@ -0,0 +1,63 @@
+﻿// Copyright 2025 Team BH.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectComponent.h"
+#include "RsAdditionalSourceEffectComponent.generated.h"
+
+struct FGameplayEffectRemovalInfo;
+struct FConditionalGameplayEffect;
+/**
+ * Apply additional Gameplay Effects to effect source (instigator)
+ */
+UCLASS(CollapseCategories)
+class RS_API URsAdditionalSourceEffectComponent : public UGameplayEffectComponent
+{
+	GENERATED_BODY()
+
+public:
+	/**
+     * Called when a Gameplay Effect is Added to the ActiveGameplayEffectsContainer.  We register a callback to execute the OnComplete Gameplay Effects.
+     */
+	virtual bool OnActiveGameplayEffectAdded(FActiveGameplayEffectsContainer& GEContainer, FActiveGameplayEffect& ActiveGE) const override;
+
+	/**
+	 * Called when a Gameplay Effect is applied.  This executes the OnApplication Gameplay Effects.
+	 */
+	virtual void OnGameplayEffectApplied(FActiveGameplayEffectsContainer& ActiveGEContainer, FGameplayEffectSpec& GESpec, FPredictionKey& PredictionKey) const override;
+
+#if WITH_EDITOR
+	/**
+	 * There are some fields that are incompatible with one another, so let's catch them during configuration phase. 
+	 */
+	virtual EDataValidationResult IsDataValid(class FDataValidationContext& Context) const override;
+#endif // WITH_EDITOR
+
+
+protected:
+	/**
+	 * Whenever the ActiveGE gets removed, we want to apply the configured OnComplete GameplayEffects
+	 */
+	void OnActiveGameplayEffectRemoved(const FGameplayEffectRemovalInfo& RemovalInfo, FActiveGameplayEffectsContainer* ActiveGEContainer) const;
+
+public:
+	// UPROPERTY(EditDefaultsOnly)
+	// bool bApplyOnce = true;
+
+	/** Other gameplay effects that will be applied to the source of this effect if the owning effect applies */
+	UPROPERTY(EditDefaultsOnly, Category = OnApplication)
+	TArray<TSubclassOf<UGameplayEffect>> OnApplicationGameplayEffects;
+
+	/** Effects to apply when this effect completes, regardless of how it ends */
+	UPROPERTY(EditDefaultsOnly, Category = OnComplete)
+	TArray<TSubclassOf<UGameplayEffect>> OnCompleteAlways;
+
+	/** Effects to apply when this effect expires naturally via its duration */
+	UPROPERTY(EditDefaultsOnly, Category = OnComplete)
+	TArray<TSubclassOf<UGameplayEffect>> OnCompleteNormal;
+
+	/** Effects to apply when this effect is made to expire prematurely (e.g. via a forced removal, clear tags, etc.) */
+	UPROPERTY(EditDefaultsOnly, Category = OnComplete)
+	TArray<TSubclassOf<UGameplayEffect>> OnCompletePrematurely;
+};
