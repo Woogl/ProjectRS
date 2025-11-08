b6b52c0c51372e6908c96da6b85307db6937b757
DESKTOP-6R1B181\user <woojoo0407@naver.com>
2025-11-08 21:55:29 +0900

    Build error fixed

--- Source/Rs/AbilitySystem/Abilities/RsGameplayAbility.cpp
+++ Source/Rs/AbilitySystem/Abilities/RsGameplayAbility.cpp
@@ -415,9 +415,7 @@ void URsGameplayAbility::HandleGameplayEvent(FGameplayEventData EventData)
 			{
 				FGameplayEffectContextHandle EffectContext = EventData.ContextHandle.IsValid() ? EventData.ContextHandle : SourceASC->MakeEffectContext();
 				FGameplayEffectSpecHandle GESpec = SourceASC->MakeOutgoingSpec(*Effect, GetAbilityLevel(), EffectContext);
-				GESpec.Data->DynamicGrantedTags.AddTag(TableRow->EffectTag);
-				GESpec.Data->DynamicGrantedTags.AddTag(TableRow->OwnedTag1);
-				GESpec.Data->DynamicGrantedTags.AddTag(TableRow->OwnedTag2);
+				GESpec.Data->AddDynamicAssetTag(TableRow->EffectTag);
 				if (GESpec.IsValid())
 				{
 					// Set table data in GE spec
