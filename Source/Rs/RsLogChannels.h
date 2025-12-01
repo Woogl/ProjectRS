db7a7cb154728273b7f8b278c139e4c7f754d322
DESKTOP-6R1B181\user <woojoo0407@naver.com>
2025-12-01 17:58:11 +0900

    LogChannel

--- Source/Rs/RsLogChannels.h
+++ Source/Rs/RsLogChannels.h
@@ -5,3 +5,5 @@
 #include "Logging/LogMacros.h"
 
 RS_API DECLARE_LOG_CATEGORY_EXTERN(RsLog, Log, All);
+RS_API DECLARE_LOG_CATEGORY_EXTERN(RsAbilityLog, Log, All);
+RS_API DECLARE_LOG_CATEGORY_EXTERN(RsUILog, Log, All);
