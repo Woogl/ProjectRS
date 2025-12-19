d77e36c3e5e32a0138e2cb428000050b82387467
DESKTOP-6R1B181\user <woojoo0407@naver.com>
2025-12-19 22:34:38 +0900

    GE_Generic

---
+++ Source/Rs/AbilitySystem/Calculation/ModifierMagnitude/RsPeriodCalculation.cpp
@@ -0,0 +1,18 @@
+﻿// Copyright 2025 Team BH.
+
+
+#include "RsPeriodCalculation.h"
+
+#include "Rs/RsLogChannels.h"
+#include "Rs/AbilitySystem/RsAbilitySystemGlobals.h"
+#include "Rs/AbilitySystem/Effect/RsEffectTable.h"
+
+float URsPeriodCalculation::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	if (const FRsEffectTableRow* Row = URsAbilitySystemGlobals::GetSetByCallerTableRow<FRsEffectTableRow>(Spec))
+	{
+		return Row->FindValue<float>(TEXT("Period"), true);
+	}
+	UE_LOG(RsAbilityLog, Warning, TEXT("Cannot find HealthDamageExpression of [%s]"), *Spec.ToSimpleString());
+	return 0.f;
+}
---
+++ Source/Rs/AbilitySystem/Calculation/ModifierMagnitude/RsPeriodCalculation.h
@@ -0,0 +1,19 @@
+﻿// Copyright 2025 Team BH.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayModMagnitudeCalculation.h"
+#include "RsPeriodCalculation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RS_API URsPeriodCalculation : public UGameplayModMagnitudeCalculation
+{
+	GENERATED_BODY()
+	
+public:
+	virtual float CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const override;
+};
