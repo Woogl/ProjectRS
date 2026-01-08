429919cea8f65ff1ab3465bac2c3ee42f775cb9f
DESKTOP-6R1B181\user <woojoo0407@naver.com>
2026-01-09 03:11:03 +0900

    Tab buttons

---
+++ Source/Rs/UI/Widget/RsTabButtonBase.cpp
@@ -0,0 +1,28 @@
+﻿// Copyright 2025 Team BH.
+
+
+#include "RsTabButtonBase.h"
+
+#include "CommonLazyImage.h"
+
+void URsTabButtonBase::SetIconFromLazyObject(TSoftObjectPtr<UObject> LazyObject)
+{
+	if (LazyImage_Icon)
+	{
+		LazyImage_Icon->SetBrushFromLazyDisplayAsset(LazyObject);
+	}
+}
+
+void URsTabButtonBase::SetIconBrush(const FSlateBrush& Brush)
+{
+	if (LazyImage_Icon)
+	{
+		LazyImage_Icon->SetBrush(Brush);
+	}
+}
+
+void URsTabButtonBase::SetTabLabelInfo_Implementation(const FRsTabDescriptor& TabLabelInfo)
+{
+	SetButtonText(TabLabelInfo.TabText);
+	SetIconBrush(TabLabelInfo.IconBrush);
+}
---
+++ Source/Rs/UI/Widget/RsTabButtonBase.h
@@ -0,0 +1,32 @@
+﻿// Copyright 2025 Team BH.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RsButtonBase.h"
+#include "RsTabListWidgetBase.h"
+#include "RsTabButtonBase.generated.h"
+
+struct FRsTabDescriptor;
+/**
+ * 
+ */
+UCLASS(Abstract, Blueprintable, meta = (DisableNativeTick))
+class RS_API URsTabButtonBase : public URsButtonBase, public IRsTabButtonInterface
+{
+	GENERATED_BODY()
+	
+public:
+	void SetIconFromLazyObject(TSoftObjectPtr<UObject> LazyObject);
+	void SetIconBrush(const FSlateBrush& Brush);
+
+protected:
+
+	UFUNCTION()
+	virtual void SetTabLabelInfo_Implementation(const FRsTabDescriptor& TabLabelInfo) override;
+
+private:
+
+	UPROPERTY(meta = (BindWidgetOptional))
+	TObjectPtr<UCommonLazyImage> LazyImage_Icon;
+};
